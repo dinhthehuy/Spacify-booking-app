f0a0ecc095d5c2c93121117b9f57120aa6660346
dinhthehuy <dinhthehuy51@gmail.com>
2023-11-19 09:16:13 +0100

    first commit

---
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/BottomNavigation.java
@@ -0,0 +1,29 @@
+package com.example.hci_prototyp_ws23.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hci_prototyp_ws23.R;
+
+public class BottomNavigation extends Fragment {
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_bottom_navigation, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
