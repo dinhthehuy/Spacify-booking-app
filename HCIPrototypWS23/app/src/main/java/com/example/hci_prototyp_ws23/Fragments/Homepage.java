f24eafcb989b6e1f72d9dd55564014142a103ffd
dinhthehuy <dinhthehuy51@gmail.com>
2023-11-20 21:13:15 +0100

    Set up Bottom Navigation Bar and its navigation

--- HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/Homepage.java
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/Homepage.java
@@ -11,19 +11,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hci_prototyp_ws23.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Homepage extends Fragment {
     View view;
+    BottomNavigationView bottomNavigationView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_homepage, container, false);
+        bottomNavigationView = requireActivity().findViewById(R.id.bottom_navigation_bar);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        bottomNavigationView.setVisibility(View.VISIBLE);
     }
 }
--- HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/MainActivity.java
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.hci_prototyp_ws23;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.hci_prototyp_ws23.Fragments.Homepage;
+import com.example.hci_prototyp_ws23.Fragments.UserProfile;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int menuItemId = item.getItemId();
+            Fragment fragment;
+
+            if(menuItemId == R.id.user_profile_item) {
+                fragment = new UserProfile();
+                navigateFromMenuItem(fragment);
+                return true;
+            } else if(menuItemId == R.id.homepage) {
+                fragment = new Homepage();
+                navigateFromMenuItem(fragment);
+                return true;
+            }
+            return true;
+        });
+    }
+
+    private void navigateFromMenuItem(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
