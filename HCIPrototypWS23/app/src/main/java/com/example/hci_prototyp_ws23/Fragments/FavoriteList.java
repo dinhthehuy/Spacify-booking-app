9b24dc5297f8611b9a94005d3b4a46b1ba4c8e48
dinhthehuy <dinhthehuy51@gmail.com>
2023-11-25 09:52:15 +0100

    Add new fragments for Bottom Navigation

---
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/FavoriteList.java
@@ -0,0 +1,33 @@
+package com.example.hci_prototyp_ws23.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hci_prototyp_ws23.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class FavoriteList extends Fragment {
+    View view;
+    BottomNavigationView bottomNavigationView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_favorite_list, container, false);
+        bottomNavigationView = requireActivity().findViewById(R.id.bottom_navigation_bar);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bottomNavigationView.setVisibility(View.VISIBLE);
+    }
+}
