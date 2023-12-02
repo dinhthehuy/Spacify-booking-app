071c0e4bb14e942f874dd08ba0ac44063448576e
dinhthehuy <dinhthehuy51@gmail.com>
2023-12-02 16:48:21 +0100

    new files

--- HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/SavedList.java
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/SavedList.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,15 +14,17 @@ import android.view.ViewGroup;
 import com.example.hci_prototyp_ws23.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class FavoriteList extends Fragment {
+public class SavedList extends Fragment {
     View view;
     BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_favorite_list, container, false);
+        view = inflater.inflate(R.layout.fragment_saved_list, container, false);
         bottomNavigationView = requireActivity().findViewById(R.id.bottom_navigation_bar);
+        toolbar = view.findViewById(R.id.saved_list_tb);
         return view;
     }
 
@@ -30,5 +33,9 @@ public class FavoriteList extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         bottomNavigationView.setVisibility(View.VISIBLE);
         bottomNavigationView.getMenu().getItem(1).setChecked(true);
+        toolbar.setVisibility(View.VISIBLE);
+        toolbar.setTitle("Saved");
+        toolbar.setTitleMarginStart(450);
+        toolbar.inflateMenu(R.menu.top_action_bar_saved);
     }
 }
