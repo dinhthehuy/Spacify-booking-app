34dff718aa5fbe6769bb09c34506b7257bbd004e
dinhthehuy <dinhthehuy51@gmail.com>
2023-12-03 17:50:03 +0100

    Add new fragments: RoomInformation, BookingOverview, BookingConfirmation

---
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/BookingConfimation.java
@@ -0,0 +1,23 @@
+package com.example.hci_prototyp_ws23.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hci_prototyp_ws23.R;
+
+public class BookingConfimation extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_booking_confimation, container, false);
+        return view;
+    }
+}
---
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/BookingOverview.java
@@ -0,0 +1,22 @@
+package com.example.hci_prototyp_ws23.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hci_prototyp_ws23.R;
+
+public class BookingOverview extends Fragment {
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_booking_overview, container, false);
+        return view;
+    }
+}
