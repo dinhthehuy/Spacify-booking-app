d4f95e9f7fad6221b282b23caf3f0af420d70cf7
dinhthehuy <dinhthehuy51@gmail.com>
2023-12-04 13:49:09 +0100

    Set up adapter and list item, no implementation yet

--- HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Adapters/UserBookingsAdapter.java
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Adapters/UserBookingsAdapter.java
@@ -1,4 +1,44 @@
 package com.example.hci_prototyp_ws23.Adapters;
 
-public class UserBookingsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hci_prototyp_ws23.Models.Hotel;
+import com.example.hci_prototyp_ws23.R;
+
+import java.util.List;
+
+public class UserBookingsAdapter extends RecyclerView.Adapter<UserBookingsAdapter.UserBookingAdapterViewHolder> {
+    List<Hotel> hotelList;
+    public UserBookingsAdapter(List<Hotel> hotelList) {
+        this.hotelList = hotelList;
+    }
+    @NonNull
+    @Override
+    public UserBookingAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_bookings_item, parent, false);
+        return new UserBookingAdapterViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserBookingAdapterViewHolder holder, int position) {
+        // Implementation needed
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class UserBookingAdapterViewHolder extends RecyclerView.ViewHolder {
+        // Implementation needed
+        public UserBookingAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // Implementation needed
+        }
+    }
 }
