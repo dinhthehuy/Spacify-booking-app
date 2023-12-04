2fa883ea7a936e95f956582b97f0f06c6e58726a
dinhthehuy <dinhthehuy51@gmail.com>
2023-12-04 13:37:54 +0100

    Set up adapter and list item, no implementation yet

--- HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Adapters/SavedListAdapter.java
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Adapters/SavedListAdapter.java
@@ -1,4 +1,44 @@
 package com.example.hci_prototyp_ws23.Adapters;
 
-public class SavedListAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hci_prototyp_ws23.Models.Hotel;
+import com.example.hci_prototyp_ws23.R;
+
+import java.util.List;
+
+public class SavedListAdapter extends RecyclerView.Adapter<SavedListAdapter.SavedListAdapterViewHolder> {
+    private List<Hotel> hotelList;
+    public SavedListAdapter(List<Hotel> hotelList) {
+        this.hotelList = hotelList;
+    }
+    @NonNull
+    @Override
+    public SavedListAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.saved_list_item, parent, false);
+        return new SavedListAdapterViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SavedListAdapterViewHolder holder, int position) {
+        // Implementation needed
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class SavedListAdapterViewHolder extends RecyclerView.ViewHolder{
+        // Implementation needed
+        public SavedListAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // Implementation needed
+        }
+    }
 }
--- HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Adapters/SearchResultListAdapter.java
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Adapters/SearchResultListAdapter.java
@@ -1,4 +1,46 @@
 package com.example.hci_prototyp_ws23.Adapters;
 
-public class SearchResultListAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hci_prototyp_ws23.Models.Hotel;
+import com.example.hci_prototyp_ws23.R;
+
+import java.util.List;
+
+public class SearchResultListAdapter extends RecyclerView.Adapter<SearchResultListAdapter.SearchResultListAdapterViewHolder> {
+    private List<Hotel> hotelList;
+    public SearchResultListAdapter(List<Hotel> hotelList) {
+        this.hotelList = hotelList;
+    }
+    @NonNull
+    @Override
+    public SearchResultListAdapter.SearchResultListAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_result_list_item, parent, false);
+        return new SearchResultListAdapterViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchResultListAdapter.SearchResultListAdapterViewHolder holder, int position) {
+        // Implementation needed
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class SearchResultListAdapterViewHolder extends RecyclerView.ViewHolder {
+        // Implementation needed
+
+        public SearchResultListAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // Implementation needed
+        }
+    }
+
 }
