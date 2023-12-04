db6bec3b08b12d9c24ef202ed8f8c60bfc9b6734
dinhthehuy <dinhthehuy51@gmail.com>
2023-12-04 19:14:35 +0100

    Add getters and setters

--- HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Models/Booking.java
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Models/Booking.java
@@ -13,7 +13,82 @@ public class Booking {
     private double totalPrice;
     private PaymentMethod paymentMethod;
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public int getAdultNumber() {
+        return adultNumber;
+    }
+
+    public void setAdultNumber(int adultNumber) {
+        this.adultNumber = adultNumber;
+    }
+
+    public int getChildrenNumber() {
+        return childrenNumber;
+    }
+
+    public void setChildrenNumber(int childrenNumber) {
+        this.childrenNumber = childrenNumber;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
     public enum PaymentMethod {
         PAYPAL, ONSITE, BANKING;
+    }
+    public Booking(User user, Hotel hotel, RoomType roomType, Date checkInDate, Date checkOutDate, int adultNumber, int childrenNumber, double totalPrice) {
+
     }
 }
