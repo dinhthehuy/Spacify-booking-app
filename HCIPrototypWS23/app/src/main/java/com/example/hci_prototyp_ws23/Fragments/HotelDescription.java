7298c56253860f6d9e2335b9bfddc6c437c7c360
dinhthehuy <dinhthehuy51@gmail.com>
2023-12-05 01:21:55 +0100

    Add top action bar to hotel description fragment

--- HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/HotelDescription.java
+++ HCIPrototypWS23/app/src/main/java/com/example/hci_prototyp_ws23/Fragments/HotelDescription.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,18 +18,24 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class HotelDescription extends Fragment {
     View view;
     BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_hotel_description, container, false);
         bottomNavigationView = requireActivity().findViewById(R.id.bottom_navigation_bar);
+        toolbar = view.findViewById(R.id.hotelDescription_tb);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        bottomNavigationView.setVisibility(View.VISIBLE);
+        bottomNavigationView.setVisibility(View.GONE);
+        toolbar.setVisibility(View.VISIBLE);
+        toolbar.setTitle("HOTEL");
+        toolbar.inflateMenu(R.menu.top_action_bar_hotel_description);
+        toolbar.setNavigationOnClickListener(v -> NavHostFragment.findNavController(HotelDescription.this).navigate(R.id.action_hotelDescription_to_searchResultList));
     }
 }
